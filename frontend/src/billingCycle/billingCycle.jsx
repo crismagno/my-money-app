d9ac26530be71f4259bd2de0142e8d86ff68ed7b
Cristhofer Magno <cristhofer.mendonca@gmail.com>
2019-05-29 17:31:49 -0300

    form-update concluido

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -11,7 +11,7 @@ import TabHeader from '../common/tab/tabHeader'
 import TabsContent from '../common/tab/tabsContent'
 import TabContent from '../common/tab/tabContent'
 import { selectTab, showTabs } from "../common/tab/tabActions"
-import { create } from "./billingCycleActions";
+import { create, update } from "./billingCycleActions";
 
 import List from "./billingCycleList"
 import Form from "./billingCycleForm"
@@ -44,7 +44,7 @@ class BillingCycles extends React.Component {
                                 <Form onSubmit={this.props.create}/>
                             </TabContent>
                             <TabContent id="tabUpdate">
-                                <Form />
+                                <Form onSubmit={this.props.update}/>
                             </TabContent>
                             <TabContent id="tabDelete"><h1>Excluir</h1></TabContent>
                         </TabsContent>
@@ -55,5 +55,5 @@ class BillingCycles extends React.Component {
     }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({ selectTab, showTabs, create }, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({ selectTab, showTabs, create, update }, dispatch)
 export default connect(null, mapDispatchToProps)(BillingCycles)
